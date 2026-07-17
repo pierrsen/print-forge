eb29c85c8a2bfc0ba04965edc26dbaf168d5716a
Piersen Zambrano <piersenzambrano@gmail.com>
2026-07-17 01:33:20 -0700

    feat: add links routing 3d-model and about navbar buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Link from "next/link";
 import { Albert_Sans, Montserrat_Alternates } from "next/font/google";
 import PFLogoIcon from "@/public/printforge-logo-icon.svg";
 import PFLogo from "@/public/printforge-logo.svg";
@@ -27,23 +28,29 @@ export default function RootLayout({
       >
         <header className="w-full bg-white">
           <nav className="flex justify-between px-6 py-4">
-            <div className="relative">
-              {/* Desktop logo */}
-              <img
-                src={PFLogo.src}
-                alt="PrintForge Logo"
-                className="w-[200px] h-auto hidden md:block"
-              />
-              {/* Mobile logo */}
-              <img
-                src={PFLogo.src}
-                alt="PrintForge Logo"
-                className="w-[40px] h-auto block md:hidden"
-              />
-            </div>
+            <Link href="/">
+              <div className="relative">
+                {/* Desktop logo */}
+                <img
+                  src={PFLogo.src}
+                  alt="PrintForge Logo"
+                  className="w-[200px] h-auto hidden md:block"
+                />
+                {/* Mobile logo */}
+                <img
+                  src={PFLogo.src}
+                  alt="PrintForge Logo"
+                  className="w-[40px] h-auto block md:hidden"
+                />
+              </div>
+            </Link>
             <ul className="flex items-center gap-2.5">
-              <p>3D Models</p>
-              <p>About</p>
+              <li className="text-sm uppercase cursor-pointer">
+                <Link href="/3d-models">3D Models</Link>
+              </li>
+              <li className="text-sm uppercase cursor-pointer">
+                <Link href="/about">About</Link>
+              </li>
             </ul>
           </nav>
         </header>
