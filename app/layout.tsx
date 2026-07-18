3ea75a66f31e01d54f752e9d9b1adff735246443
Piersen Zambrano <piersenzambrano@gmail.com>
2026-07-17 19:49:27 -0700

    feat: add 3d models page and 3d-model-id dynamic routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Link from "next/link";
 import type { RootLayoutProps } from "@/app/types";
+import Navbar from "@/app/components/Navbar";
 import { Albert_Sans, Montserrat_Alternates } from "next/font/google";
 import PFLogoIcon from "@/public/printforge-logo-icon.svg";
 import PFLogo from "@/public/printforge-logo.svg";
@@ -23,34 +24,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body
         className={`${albertSans.className} ${montserratAlternates.variable}`}
       >
-        <header className="w-full bg-white">
-          <nav className="flex justify-between px-6 py-4">
-            <Link href="/">
-              <div className="relative">
-                {/* Desktop logo */}
-                <img
-                  src={PFLogo.src}
-                  alt="PrintForge Logo"
-                  className="w-[200px] h-auto hidden md:block"
-                />
-                {/* Mobile logo */}
-                <img
-                  src={PFLogo.src}
-                  alt="PrintForge Logo"
-                  className="w-[40px] h-auto block md:hidden"
-                />
-              </div>
-            </Link>
-            <ul className="flex items-center gap-2.5">
-              <li className="text-sm uppercase cursor-pointer">
-                <Link href="/3d-models">3D Models</Link>
-              </li>
-              <li className="text-sm uppercase cursor-pointer">
-                <Link href="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        <Navbar />
         {children}
       </body>
     </html>
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,3 +1,30 @@
+import type { ReactNode } from "react"
+
 export type RootLayoutProps = Readonly<{
   children: React.ReactNode;
 }>
+
+export type Model = {
+  id: number
+  name: string
+  description: string
+  likes: number
+  image: string
+  category: string
+  dateAdded: string
+}
+
+export type ModelDetailPageProps = {
+    params: Promise<{
+        id: string
+    }>
+}
+
+export type ModelCardProps = {
+    model: Model
+}
+
+export type PillProps = {
+    children: ReactNode
+    className?: string
+}
